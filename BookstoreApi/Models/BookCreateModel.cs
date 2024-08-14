cb30793d1ce24bdd374588e425411ba9a92b7986
Santhoshi Pavani A <santhoshipavani-all@pluralsight.com>
2024-08-14 12:53:16 +0530

    added namespace - minor change

--- BookstoreApi/Models/BookCreateModel.cs
+++ BookstoreApi/Models/BookCreateModel.cs
@@ -1,10 +1,13 @@
 // Models/BookCreateModel.cs
-public class BookCreateModel
+namespace BookstoreApi.Models
 {
-    public string? title { get; set; }
-    public decimal price { get; set; }
-    public DateTime publication_date { get; set; }
-    public int author_id { get; set; }
-    public int genre_id { get; set; }
+    public class BookCreateModel
+    {
+        public string? title { get; set; }
+        public decimal price { get; set; }
+        public DateTime publication_date { get; set; }
+        public int author_id { get; set; }
+        public int genre_id { get; set; }
 
+    }
 }
